319e300507f50e8e50a53b8a8f7530bfbd8a7a80
Will Tabor <tabor.will@gmail.com>
2016-04-01 12:50:03 -0400

    Update

---
+++ javascript/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+      var board = [];
+      var xWinCount = 0;
+      var oWincount = 0;
+      var turn = 0;
+
+
+      for (i = 0; i < 8, i++) {
+        board.push('null');
+      }
+
+      $('#square1').click(function() {
+      if ($('#dropUnit').is(':enabled')) {
+        if (turn % )
+        $(#square1).textContent('X');
+      }
+
+              if (player1 == true);
+              board[0][0] = 'X';
+              $(#square1).textContent('X');
+            } else {
+              board[0][0] = 'O';
+              $(#square1).textContent('O');
+            } else {
+              $('#square1').attr(':disabled');
+              alert("Square has been taken");
+            }
+
+            function Counter() {}
+
+
+
+
+
+          }
