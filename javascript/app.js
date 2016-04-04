690a55b66880e434242a795cbc8b81dc5f10e4aa
Will Tabor <tabor.will@gmail.com>
2016-04-04 13:11:38 -0400

    Game working, needs styling

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,75 @@
 $(document).ready(function() {
-      var board = [];
-      var xWinCount = 0;
-      var oWincount = 0;
-      var turn = 0;
-
-
-      for (i = 0; i < 8, i++) {
-        board.push('null');
-      }
-
-      $('#square1').click(function() {
-      if ($('#square1').is(':enabled')) {
-        if (turn % 0 == 0) {
-          //Turn player X
-        $('#square1').textContent('X');
-        $('#square1').attr(':disabled');
-        var board[0] = true;
-        turn++;
-        }
-
-      }
-
-              if (player1 == true);
-              board[0][0] = 'X';
-              $(#square1).textContent('X');
-            } else {
-              board[0][0] = 'O';
-              $(#square1).textContent('O');
-            } else {
-              $('#square1').attr(':disabled');
-              alert("Square has been taken");
-            }
-
-            function Counter() {}
 
 
 
+  // Variables
+  var $gameSquares = $('.square');
+  var moves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var counter = 0;
+  var xWinsCounter = 0;
+  var oWinsCounter = 0;
 
+  // Functions
 
-          }
+  //Remove or reset?
+  function resetBoard() {
+    if (xWinsCounter && oWinsCounter < 5) {
+      moves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      counter = 0;
+      $gameSquares.removeClass('x-image o-image');
+      $(gameSquares).unbind('click');
+    }
+    else {
+      alert('Series over!');
+    }
+  }
+
+  function getWin() {
+    var winConditions = [moves[0] + moves[1] + moves[2],
+      moves[3] + moves[4] + moves[5],
+      moves[6] + moves[7] + moves[8],
+      moves[0] + moves[4] + moves[8],
+      moves[0] + moves[3] + moves[6],
+      moves[1] + moves[4] + moves[7],
+      moves[2] + moves[5] + moves[8],
+      moves[2] + moves[4] + moves[6]
+    ]
+    console.log(winConditions);
+    for (i = 0; i < winConditions.length; i++) {
+      console.log(winConditions[i]);
+      if (winConditions[i] === 3) {
+        alert('X Wins!');
+        xWinsCounter++;
+        $('#xwins').text(xWinsCounter); //Update X Wins
+        resetBoard(); //Run reset function
+      } else if (winConditions[i] === 30) {
+        alert('O Wins!');
+        oWinsCounter++;
+        $('#owins').text(oWinsCounter); //Update O Wins
+        resetBoard(); //Run reset function
+      } else if (counter == 9) {
+        console.log('tie!');
+        alert('The game is a tie!');
+        resetBoard();
+      } else {}
+    }
+  }
+
+  //On Click
+  $($gameSquares).on('click', function() {
+    var id = parseInt(this.id);
+    moves[this.id] = id;
+    if (counter % 2 == 0) {
+      moves[this.id] = +1;
+      $(this).addClass('x-image'); //Change cell to X image/img.png
+      console.log('x')
+    } else {
+      moves[this.id] = +10;
+      $(this).addClass('o-image'); //Change cell to O image/img.png
+      console.log('o');
+    }
+    counter++;
+    console.log(moves);
+    getWin();
+  });
+});
