87640b472ed187d800640194285092e28ff7285f
Will Tabor <tabor.will@gmail.com>
2016-04-05 13:22:16 -0400

    Added reset button and current player

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
 
   // Variables
+
   var $gameSquares = $('.square');
   var moves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   var counter = 0;
   var xWinsCounter = 0;
   var oWinsCounter = 0;
+  var X = 'X';
+  var O = 'O';
+  var cPlayer = X;
 
   // Functions
 
+  var displayMessage = function(message) {
+    $('.message').html(message);
+  };
+
   function resetBoard() {
-    if (xWinsCounter && oWinsCounter < 5) {
+    if (xWinsCounter < 5 && oWinsCounter < 5) {
       moves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
       counter = 0;
+      displayMessage('Current Player: ' + X);
+      //Clears cell content
       $gameSquares.removeClass('x-image o-image');
-      $(gameSquares).unbind('click');
     } else {
-      alert('Series over!');
+      alert('The series is over!');
+      $('.square').off('click');
+      $('.square').hide();
+      $('.message').hide();
+      $('.reset').show().on('click', function() {
+        location.reload();
+      });
     }
   }
 
@@ -35,14 +50,19 @@ $(document).ready(function() {
       if (winConditions[i] === 3) {
         alert('X Wins!');
         xWinsCounter++;
-        $('#xwins').text(xWinsCounter); //Update X Wins
-        resetBoard(); //Run reset function
+        //Update X Wins
+        $('#xwins').text(xWinsCounter);
+        //Run reset function
+        resetBoard();
       } else if (winConditions[i] === 30) {
         alert('O Wins!');
         oWinsCounter++;
-        $('#owins').text(oWinsCounter); //Update O Wins
-        resetBoard(); //Run reset function
+        //Update O Wins
+        $('#owins').text(oWinsCounter);
+        //Run reset function
+        resetBoard();
       } else if (counter == 9) {
+        //Runs if all squares have been clicked
         console.log('tie!');
         alert('The game is a tie!');
         resetBoard();
@@ -52,22 +72,22 @@ $(document).ready(function() {
 
   //On Click
   $($gameSquares).on('click', function() {
-    console.log(moves[this.id]);
+    //Determines if this cell has been clicked already
     if (moves[this.id] === 0) {
-      var id = parseInt(this.id);
-      console.log(id);
-      moves[this.id] = id;
       if (counter % 2 == 0) {
+        //Changes value at moves[i] to 1
         moves[this.id] = +1;
-        $(this).addClass('x-image'); //Change cell to X image/img.png
-
+        //Change cell to X image
+        $(this).addClass('x-image');
+        displayMessage('Current Player: ' + O);
       } else {
+        //Changes value at moves[i] to 10
         moves[this.id] = +10;
-        $(this).addClass('o-image'); //Change cell to O image/img.png
-
+        //Changes cell O image
+        $(this).addClass('o-image');
+        displayMessage('Current Player: ' + X);
       }
       counter++;
-
       getWin();
     } else if (this.id !== 0)
       alert("Select a different square");
